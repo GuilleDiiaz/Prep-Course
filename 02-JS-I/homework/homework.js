a722fdc1f79f42650084be47b7530bd1da838267
GuilleDiiaz <locoguille313@gmail.com>
2022-08-30 17:29:31 -0300

    resultado l35-43-50

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -36,19 +36,22 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+var resultado = x+y;
+return resultado
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  var resta = x-y;
+  return resta
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  var multiplica = x*y;
+  return multiplica
 }
 
 function divide(x, y) {
